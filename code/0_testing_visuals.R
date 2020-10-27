f5e6ab692e0cd42b59bc70178d249ae46dfadf1f
yurikimbmgf <yuri.kim@gatesfoundation.org>
2020-10-26 21:15:34 -0700

    got all my visuals set. now need to apply it..

--- code/0_testing_visuals.R
+++ code/0_testing_visuals.R
@@ -36,7 +36,8 @@ test <- tabyl(test_4, comfort_grocery) %>%
   rename(choice = comfort_grocery) %>% 
   mutate(bar_color = case_when(choice == test_4 %>% filter(timestamp == max(timestamp)) %>% select(comfort_grocery) %>% as.numeric() ~ "#e88f00",
                                TRUE ~ "#124d47")) %>% 
-  mutate(your_selection = case_when(choice == test_4 %>% filter(timestamp == max(timestamp)) %>% select(comfort_grocery) %>% as.numeric() ~ "Your Pick"))
+  mutate(your_selection = case_when(choice == test_4 %>% filter(timestamp == max(timestamp)) %>% select(comfort_grocery) %>% as.numeric() ~ paste0(percent(percent, accuracy = 1), " (Your pick)"),
+                                    TRUE ~ percent(percent, accuracy = 1)))
 
 
 test_4 %>% filter(timestamp == max(timestamp)) %>% select(comfort_grocery) %>% as.numeric()
@@ -52,4 +53,36 @@ ggplot(data = test,
   scale_color_identity() +
   scale_y_continuous(label = percent) +
   theme_fivethirtyeight() +
+  coord_cartesian(ylim = c(0, 1)) +
   ggtitle("Test")
+
+
+
+# behavior ----------------------------------------------------------------
+dat
+test_b <- dat %>% select(timestamp, behavior_personal)
+test_b
+
+test_b_pct <- tabyl(test_b, behavior_personal) %>% 
+  rename(choice = behavior_personal) %>% 
+  mutate(bar_color = case_when(choice == test_b %>% filter(timestamp == max(timestamp)) %>% select(behavior_personal) %>% as.character() ~ "#e88f00",
+                               TRUE ~ "#124d47")) %>% 
+  mutate(your_selection = case_when(choice == test_b %>% filter(timestamp == max(timestamp)) %>% select(behavior_personal) %>% as.character() ~ paste0(percent(percent, accuracy = 1), " (Your pick)"),
+                                    TRUE ~ percent(percent, accuracy = 1)))
+
+
+ggplot(data = test_b_pct,
+       aes(x = choice, 
+           y = percent,
+           fill = bar_color,
+           color = bar_color,
+           label = your_selection)) +
+  geom_bar(stat = "identity") +
+  geom_text(vjust = -1) +
+  scale_fill_identity() +
+  scale_color_identity() +
+  scale_y_continuous(label = percent) +
+  theme_fivethirtyeight() +
+  coord_cartesian(ylim = c(0, 1)) +
+  ggtitle("Test")
+
